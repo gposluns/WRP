bb3e9b11fcd089ee56f482ea3de70d6293854094
noot <elizabethjbinks@gmail.com>
2020-02-24 14:16:22 -0500

    implement mv that copies file contents

--- UserProgram/wrp_mv.c
+++ UserProgram/wrp_mv.c
@@ -4,15 +4,52 @@ int mv_simple(const char *oldpath, const char *newpath) {
 	return rename(oldpath, newpath);
 }
 
+int mv(const char *oldpath, const char *newpath) {
+	FILE *oldptr;
+	FILE *newptr;
+
+	oldptr = fopen(oldpath, "r");
+
+	newptr = fopen(newpath, "r");
+	if (newptr != NULL) {
+		printf("%s already exists", newpath);
+		return 1;
+	}
+
+	newptr = fopen(newpath, "wb");
+
+	if (oldptr == NULL) {
+		printf("%s does not exist\n", oldpath);
+		return 1;
+	} 
+
+	if (newptr == NULL) {
+		printf("%s cannot be created\n", newpath);
+		return 1;
+	}
+
+	char c;
+	while ( (c = fgetc(oldptr)) != EOF ) {
+		fputc(c, newptr);
+	}
+
+	fclose(oldptr);
+	fclose(newptr);
+
+	remove(oldpath);
+
+	return 0;
+}
 
 int main (int argc, char **argv)
 {
 	printf("wrp_mv\n");
 	if (argc != 3) {
 		printf("usage: wrp_mv /path/to/file/source /path/to/file/destination\n");
+		return 0;
 	}
 
-	if (mv_simple(argv[1], argv[2]) == 0) {
+	if (mv(argv[1], argv[2]) == 0) {
 		printf("moved %s to %s\n", argv[1], argv[2]);
 	} else {
 		printf("failed to move\n");
