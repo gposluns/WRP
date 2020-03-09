170622e648f520e065c46f4aa2226f980fbd9b8b
noot <elizabethjbinks@gmail.com>
2020-03-09 14:26:17 -0400

    add SCSI command formatting

--- UserProgram/libusb_example.c
+++ UserProgram/libusb_example.c
@@ -1,18 +1,86 @@
 #include <libusb.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
 
 #define FMT_ERROR_LD "error=%ld\n"
-#define PRINT_ERR(r) printf("error=%s\n", libusb_error_name(r))
+#define PRINT_ERR(msg, r) printf("%s: error=%s\n", msg, libusb_error_name(r))
 
 #define LUFA_VENDOR_ID 1003
 #define LUFA_PRODUCT_ID 8261
 
 //#define DEBUG 1
 
+#define RETRY_MAX                     5
+#define REQUEST_SENSE_LENGTH          0x12
+#define INQUIRY_LENGTH                0x24
+
+#define SCSI_CMD_WRITE_10                              0x2A
+#define SCSI_CMD_READ_10                               0x28
+
+/** Mask for a Command Block Wrapper's flags attribute to specify a command with data sent from host-to-device. */
+#define COMMAND_DIRECTION_DATA_OUT (0 << 7)
+
+/** Mask for a Command Block Wrapper's flags attribute to specify a command with data sent from device-to-host. */
+#define COMMAND_DIRECTION_DATA_IN  (1 << 7)
+
+
+// Section 5.1: Command Block Wrapper (CBW)
+struct command_block_wrapper {
+	uint8_t dCBWSignature[4];
+	uint32_t dCBWTag;
+	uint32_t dCBWDataTransferLength;
+	uint8_t bmCBWFlags;
+	uint8_t bCBWLUN;
+	uint8_t bCBWCBLength;
+	uint8_t CBWCB[16];
+};
+
+// Section 5.2: Command Status Wrapper (CSW)
+struct command_status_wrapper {
+	uint8_t dCSWSignature[4];
+	uint32_t dCSWTag;
+	uint32_t dCSWDataResidue;
+	uint8_t bCSWStatus;
+};
+
+static uint8_t cdb_length[256] = {
+//	 0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+	06,06,06,06,06,06,06,06,06,06,06,06,06,06,06,06,  //  0
+	06,06,06,06,06,06,06,06,06,06,06,06,06,06,06,06,  //  1
+	10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,  //  2
+	10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,  //  3
+	10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,  //  4
+	10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,  //  5
+	00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,  //  6
+	00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,  //  7
+	16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,  //  8
+	16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,16,  //  9
+	12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,  //  A
+	12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,  //  B
+	00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,  //  C
+	00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,  //  D
+	00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,  //  E
+	00,00,00,00,00,00,00,00,00,00,00,00,00,00,02,00,  //  F
+};
+
+static int perr(char const *format, ...)
+{
+	va_list args;
+	int r;
+
+	va_start (args, format);
+	r = vfprintf(stderr, format, args);
+	va_end(args);
+
+	return r;
+}
+
 void print_dev(libusb_device *dev, struct libusb_device_descriptor * desc, int i) {
 	int r = libusb_get_device_descriptor(dev, desc);
 	if (r < 0)  {
-		PRINT_ERR(r);
+		PRINT_ERR("cannot get device descriptor", r);
 		return;
 	}
 
@@ -63,6 +131,123 @@ void print_dev(libusb_device *dev, struct libusb_device_descriptor * desc, int i
 	return;
 }
 
+static int get_mass_storage_status(libusb_device_handle *handle, uint8_t endpoint, uint32_t expected_tag)
+{
+	int i, r, size;
+	struct command_status_wrapper csw;
+
+	// The device is allowed to STALL this transfer. If it does, you have to
+	// clear the stall and try again.
+	i = 0;
+	do {
+		r = libusb_bulk_transfer(handle, endpoint, (unsigned char*)&csw, 13, &size, 1000);
+		if (r == LIBUSB_ERROR_PIPE) {
+			libusb_clear_halt(handle, endpoint);
+		}
+		i++;
+	} while ((r == LIBUSB_ERROR_PIPE) && (i<RETRY_MAX));
+	if (r != LIBUSB_SUCCESS) {
+		perr("   get_mass_storage_status: %s\n", libusb_strerror((enum libusb_error)r));
+		return -1;
+	}
+	if (size != 13) {
+		perr("   get_mass_storage_status: received %d bytes (expected 13)\n", size);
+		return -1;
+	}
+	if (csw.dCSWTag != expected_tag) {
+		perr("   get_mass_storage_status: mismatched tags (expected %08X, received %08X)\n",
+			expected_tag, csw.dCSWTag);
+		return -1;
+	}
+	// For this test, we ignore the dCSWSignature check for validity...
+	printf("   Mass Storage Status: %02X (%s)\n", csw.bCSWStatus, csw.bCSWStatus?"FAILED":"Success");
+	if (csw.dCSWTag != expected_tag)
+		return -1;
+	if (csw.bCSWStatus) {
+		// REQUEST SENSE is appropriate only if bCSWStatus is 1, meaning that the
+		// command failed somehow.  Larger values (2 in particular) mean that
+		// the command couldn't be understood.
+		if (csw.bCSWStatus == 1) {
+			printf("CSW DataResidue:%d\n", csw.dCSWDataResidue);
+			return -2;	// request Get Sense
+		}
+		else
+			return -1;
+	}
+
+	// do {
+	// 	uint8_t b;
+	// 	r = libusb_bulk_transfer(handle, endpoint, &b, 1, &size, 1000);
+	// 	// if (r == LIBUSB_ERROR_PIPE) {
+	// 	// 	libusb_clear_halt(handle, endpoint);
+	// 	// }
+	// 	// i++;
+	// } while ((r == LIBUSB_ERROR_PIPE));
+
+	// In theory we also should check dCSWDataResidue.  But lots of devices
+	// set it wrongly.
+	return 0;
+}
+
+static int send_mass_storage_command(libusb_device_handle *handle, uint8_t endpoint, 
+	uint8_t *cdb, uint8_t direction, int data_length, uint32_t *ret_tag)
+{
+	static uint32_t tag = 1;
+	uint8_t cdb_len;
+	int i, r, size;
+	struct command_block_wrapper cbw;
+
+	if (cdb == NULL) {
+		return -1;
+	}
+
+	if (endpoint & LIBUSB_ENDPOINT_IN) {
+		perr("send_mass_storage_command: cannot send command on IN endpoint\n");
+		return -1;
+	}
+
+	cdb_len = cdb_length[cdb[0]];
+	if ((cdb_len == 0) || (cdb_len > sizeof(cbw.CBWCB))) {
+		perr("send_mass_storage_command: don't know how to handle this command (%02X, length %d)\n",
+			cdb[0], cdb_len);
+		return -1;
+	}
+	// if (cdb[0] == 0xFE) /* vendor */
+	// 	if (cdb[1] == 2) /* special command length*/
+	// 		cdb_len = 6;
+
+	memset(&cbw, 0, sizeof(cbw));
+	cbw.dCBWSignature[0] = 'U';
+	cbw.dCBWSignature[1] = 'S';
+	cbw.dCBWSignature[2] = 'B';
+	cbw.dCBWSignature[3] = 'C';
+	*ret_tag = tag;
+	cbw.dCBWTag = tag++;
+	cbw.dCBWDataTransferLength = data_length;
+	cbw.bmCBWFlags = direction;
+	cbw.bCBWLUN = 0;
+	// Subclass is 1 or 6 => cdb_len
+	cbw.bCBWCBLength = cdb_len;
+	memcpy(cbw.CBWCB, cdb, cdb_len);
+
+	i = 0;
+	do {
+		// The transfer length must always be exactly 31 bytes.
+		r = libusb_bulk_transfer(handle, endpoint, (unsigned char*)&cbw, 31, &size, 1000);
+		if (r == LIBUSB_ERROR_PIPE) {
+			libusb_clear_halt(handle, endpoint);
+		}
+		i++;
+	} while ((r == LIBUSB_ERROR_PIPE) && (i<RETRY_MAX));
+	if (r != LIBUSB_SUCCESS) {
+		perr("   send_mass_storage_command: %s\n", libusb_strerror((enum libusb_error)r));
+		return -1;
+	}
+
+	printf("   sent %d CDB bytes\n", cdb_len);
+	return 0;
+}
+
 int main (int argc, char **argv) {
 	int r;
 
@@ -72,7 +257,7 @@ int main (int argc, char **argv) {
 
 	r = libusb_init(&ctx);
 	if (r < 0) {
-		PRINT_ERR(r);
+		PRINT_ERR("cannot init libusb", r);
 		return 1;
 	}
 
@@ -98,7 +283,7 @@ int main (int argc, char **argv) {
     struct libusb_device_handle *dh = NULL;
     r = libusb_open(dev, &dh);
 	if (r < 0) {
-		PRINT_ERR(r);
+		PRINT_ERR("cannot open device", r);
 		return 1;
 	} else {
 		printf("device opened!\n");
@@ -108,11 +293,24 @@ int main (int argc, char **argv) {
         printf("error: cannot connect to device %d\n", libusb_get_device_address(dev));
     }
 
-    char data[4];
+    char * writeData = (char *)malloc(sizeof(char *)*(512+10));
+    char * readData = (char *)malloc(sizeof(char *)*(512+10));
 
-    data[0]='a';data[1]='b';data[2]='c';data[3]='d';
+    writeData[0]=SCSI_CMD_WRITE_10;
+    int totalBlocks = 1;
+    int blockAddress = 0x00000000;
+    char * data = (char *)malloc(sizeof(char *)*512);
+    strcpy(data, "noot");
 
-    int actual; //used to find out how many bytes were written
+   	memcpy(writeData+2, &blockAddress, 4);
+   	memcpy(writeData+7, &totalBlocks, 2);
+   	memcpy(writeData+9, data, 512);
+
+   	readData[0] = SCSI_CMD_READ_10;
+   	memcpy(readData+2, &blockAddress, 4);
+   	memcpy(readData+7, &totalBlocks, 2);
+
+    //int actual; //used to find out how many bytes were written
     if(libusb_kernel_driver_active(dh, 0) == 1) { //find out if kernel driver is attached
     	printf("kernel driver active\n");
     	if(libusb_detach_kernel_driver(dh, 0) == 0) //detach it
@@ -121,29 +319,59 @@ int main (int argc, char **argv) {
 
     r = libusb_claim_interface(dh, 0); //claim interface 0 (the first) of device
 	if (r < 0) {
-		PRINT_ERR(r);
+		PRINT_ERR("cannot claim interface", r);
 		return 1;
 	} else {
 		printf("interface claimed!\n");
 	}
 
-	// TODO: find read/write endpoints
-	r = libusb_bulk_transfer(dh, (2 | LIBUSB_ENDPOINT_OUT), data, 4, &actual, 0);
+	// endpoints: 4 = write and 131 = read ?
+	int endpoint_out = 4;
+	int endpoint_in = 131;
+	uint32_t expected_tag;
+
+	r = send_mass_storage_command(dh, endpoint_out, writeData, COMMAND_DIRECTION_DATA_OUT, 522, &expected_tag);
 	if (r < 0) {
-		PRINT_ERR(r);
+		printf("cannot write data\n");
 		return 1;
 	} else {
 		printf("write successful!\n");
-	}	
+	}
+
+	r = get_mass_storage_status(dh, endpoint_in, expected_tag);
+	if (r < 0) {
+		return 1;
+	} else {
+		printf("got status!\n");
+	}
+
+	r = send_mass_storage_command(dh, endpoint_out, readData, COMMAND_DIRECTION_DATA_IN, 522, &expected_tag);
+	if (r < 0) {
+		PRINT_ERR("cannot read data", r);
+		return 1;
+	} else {
+		printf("read successful!\n");
+	 	printf("readData: %s\n", readData);
+	}
+
+	r = get_mass_storage_status(dh, endpoint_in, expected_tag);
+	if (r < 0) {
+		return 1;
+	} else {
+		printf("got status!\n");
+	}
 
 	r = libusb_release_interface(dh, 0); 
 	if (r < 0) {
-		PRINT_ERR(r);
+		PRINT_ERR("cannot release interface", r);
 		return 1;
 	} else {
 		printf("released interface\n");
 	}	
 
+	free(readData);
+	free(writeData);
+	free(data);
 	libusb_close(dh);
  	libusb_free_device_list(devs, 1); // free device list
  	libusb_exit(ctx); // free ctx
